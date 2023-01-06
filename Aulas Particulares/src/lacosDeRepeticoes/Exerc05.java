92dbc55ac8f1420db1aebf5ca77eca1e16b4fbd2
Murilo César Ferreira <mucefe@yahoo.com.br>
2023-01-06 16:05:04 -0300

    Adicionei o exercício 5

---
+++ Aulas Particulares/src/lacosDeRepeticoes/Exerc05.java
@@ -0,0 +1,14 @@
+	package lacosDeRepeticoes;
+
+	public class Exerc05 {
+
+		public static void main(String[] args) {
+			
+			int i=0;
+			for (i=100;i<=200;i++) {
+				System.out.println(i);
+			}
+		}
+	}
+
+
