c19773c755b180177c879a8bac2774dd750b9673
Murilo César Ferreira <mucefe@yahoo.com.br>
2023-01-07 15:53:19 -0300

    Adicionei o exercício 02

---
+++ Aulas Particulares/src/lacosDeRepeticoes/Exerc02.java
@@ -0,0 +1,12 @@
+package lacosDeRepeticoes;
+
+public class Exerc02 {
+
+	public static void main(String[] args) {
+	for (int i=1;i<50;i++) {
+		if (i%2!=0) {
+			System.out.println(i);
+		}
+	}
+		} 
+	}
