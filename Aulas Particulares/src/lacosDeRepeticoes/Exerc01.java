c1591c52fbfd6258fd47c863e94217f5c2eed19f
Murilo César Ferreira <mucefe@yahoo.com.br>
2023-01-07 15:55:18 -0300

    Adicionei o exercício 01

---
+++ Aulas Particulares/src/lacosDeRepeticoes/Exerc01.java
@@ -0,0 +1,18 @@
+package lacosDeRepeticoes;
+
+
+public class Exerc01 {
+
+	public static void main(String[] args) {
+	int i=1;
+		for (i=1;i<21;i++) {
+			System.out.println(i);
+				
+			}
+				
+			
+		} 
+		
+		
+		
+	}
