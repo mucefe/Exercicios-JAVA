64a2b031fe754ae5638185d603a31e7f274faae5
Murilo César Ferreira <mucefe@yahoo.com.br>
2023-01-15 00:30:17 -0300

    Alterações na resolução do exercício 16

--- Aulas Particulares/src/lacosDeRepeticoes/Exerc17.java
+++ Aulas Particulares/src/lacosDeRepeticoes/Exerc17.java
@@ -1,18 +1,27 @@
 package lacosDeRepeticoes;
 import java.util.Arrays;
-public class Exerc17 {
-//Código copiado da internet
-public static void main(String[] args) {
-int[] numeros = new int[]{20,2, 8, 19, 100, 5, 1, 6, 13};
-System.out.println("Números não ordenados:");
-for(int numero : numeros)
-System.out.println(numero);
-System.out.println();
-Arrays.sort(numeros);
-System.out.println("Números ordenados:");
-for(int numero : numeros) //o que significa essa linha?
-System.out.println(numero);
-System.out.println();
-}
-}
 
+
+public class Exerc17 {
+	
+	public static void main(String[] args) {
+		boolean continuar = false;
+		int[] numeros = new int[] { 20, 2, 8, 19, 100, 5, 1, 6, 13 };
+
+		do {
+			continuar = false;
+			for (int i = 0; i < numeros.length - 1; i++) {
+				if (numeros[i] > numeros[i + 1]) {
+					int auxiliar = numeros[i];
+					numeros[i] = numeros[i + 1];
+					numeros[i + 1] = auxiliar;
+					continuar = true;
+				}
+
+			}
+		} while (continuar == true);
+		System.out.println(Arrays.toString(numeros));
+
+
+	}
+}
