312d8a4d8d2734fdb717b07c9553e92b1a8f99a3
Murilo César Ferreira <mucefe@yahoo.com.br>
2023-01-15 00:29:30 -0300

    Alterações na resolução do exercício 16

--- Aulas Particulares/src/lacosDeRepeticoes/Exerc16.java
+++ Aulas Particulares/src/lacosDeRepeticoes/Exerc16.java
@@ -1,26 +1,38 @@
 package lacosDeRepeticoes;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exerc16 {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		int[] vetor = {3,4,12,8,8,7,7,7,15,23,5};
-		
-		
-		
-		for (int i=0; i<vetor.length;i++)
-			if (vetor[i]>0) {
-				if (vetor[i]==vetor[i-1]) {
-					vetor[i]=0;
-					System.out.println(vetor[i]);
-				}
+		int[] vetor = { 3, 4, 12, 8, 8, 7, 7, 15, 7, 23, 5 };
+		int zeros = 0;
+
+		Arrays.sort(vetor);
+		for (int i = 0; i < vetor.length - 1; i++) {
+			if (vetor[i] == vetor[i + 1]) {
+				vetor[i] = 0;
+
 			}
-			
-			
-	sc.close();
+
+		}
+		Arrays.sort(vetor);
+		for (int i = 0; i < vetor.length; i++) {
+			if (vetor[i] == 0) {
+				zeros++;
+			}
+		}
+		int[] novoVetor = new int[vetor.length - zeros];
+
+		for (int i = 0; i < novoVetor.length; i++) {
+			novoVetor[i] = vetor[i + zeros];
+		}
+
+		System.out.println(Arrays.toString(novoVetor));
+		sc.close();
 	}
 
 }
