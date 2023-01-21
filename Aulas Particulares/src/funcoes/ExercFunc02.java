0e41bc96b722de21e508e0f2f047dfaf859b2d43
Murilo César Ferreira <mucefe@yahoo.com.br>
2023-01-21 15:52:23 -0300

    Adicionei o exercício 02

--- Aulas Particulares/src/funcoes/ExercFunc02.java
+++ Aulas Particulares/src/funcoes/ExercFunc02.java
@@ -22,7 +22,7 @@ public class ExercFunc02 {
 	}
 
 	public static double hipotenusa(double lado1, double lado2) {
-		double hipotenusa = Math.sqrt((lado1 * lado1) + (lado2 * lado2));
+		double hipotenusa = Math.sqrt((Math.pow(lado1,2)+Math.pow(lado2, 2)));
 		return hipotenusa;
 	}
 
