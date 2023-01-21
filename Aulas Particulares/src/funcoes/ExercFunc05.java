f0c3083cfa7a9ab14f80f2142a9e3ccd32ec03a6
Murilo César Ferreira <mucefe@yahoo.com.br>
2023-01-21 15:53:41 -0300

    Adicionei o exercício 05

--- Aulas Particulares/src/funcoes/ExercFunc05.java
+++ Aulas Particulares/src/funcoes/ExercFunc05.java
@@ -1,35 +1,41 @@
 /*Implemente um programa que lê um vetor de inteiros e um método estático que recebe esse vetor
 como parâmetro e ordena esse vetor (tente n˜ao usar Arrays.sort()).*/
+
 //Está ordenando os vetores errado. Por que?
+
 package funcoes;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ExercFunc05 {
-	static int[] vetor = new int[4]; // Não sei pq do 'static'. Só coloquei o que o Eclipse sugeriu
+	
 
 	public static void main(String[] args) {
+		int[] vetor = new int[4]; // Não sei pq do 'static'. Só coloquei o que o Eclipse sugeriu
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Insira o primeiro dos 4 números inteiros do vetor: ");
-		vetor[0] = sc.nextInt();
-		System.out.print("Insira o segundo dos 4 números inteiros do vetor: ");
-		vetor[1] = sc.nextInt();
-		System.out.print("Insira o terceiro dos 4 números inteiros do vetor: ");
-		vetor[2] = sc.nextInt();
-		System.out.print("Insira o último número inteiro do vetor: ");
-		vetor[3] = sc.nextInt();
-		System.out.println("Os números inseridos no vetor são: " + vetor[0] + ", " + vetor[1] + ", " + vetor[2] + ", "
-				+ vetor[3] + ".");
-		ordenaVetor();
-		System.out.println("Números ordenados: "+vetor[0]+", "+vetor[1]+", "+vetor[2]+", "+vetor[3]+".");
+		
+		for (int i=0;i<vetor.length;i++) {
+			System.out.print("Insira um número: ");
+			vetor[i] = sc.nextInt();
+		}
+		System.out.println("Os números inseridos no vetor são: "+Arrays.toString(vetor));
+		ordenaVetor(vetor);
+		System.out.println("Números ordenados: "+Arrays.toString(vetor));
+		int n=2;
+		recebeInt(n);
+		System.out.println(n);
+		n=recebeInt(n);
+		System.out.println(n);
+sc.close();
 	}
 
-	public static int[] ordenaVetor() {
-		boolean trocar = false;
+	public static int[] ordenaVetor(int[] vetor) {
+		boolean trocar;
 		int auxiliar = 0;
-		for (int i = 0; i < vetor.length - 1; i++) {
-			trocar = false;
-			for (int j = 0; j < vetor.length - i - 1; j++) {
+		do {
+			trocar=false;
+			for (int j = 0; j < vetor.length - 1; j++) {
 				if (vetor[j] > vetor[j + 1]) {
 					auxiliar = vetor[j];
 					vetor[j] = vetor[j + 1];
@@ -37,11 +43,14 @@ public class ExercFunc05 {
 					trocar = true;
 				}
 			}
-			if (trocar = true) {
-				break;
-			}
-		}
+			
+		}while (trocar==true);
+		
 		return vetor;
+	}
+	public static int recebeInt(int e) {
+	
+		return e+1;
 	}
 	
 
