c0a7cee5e53a1da53a5443a5d3f4bce52843fa49
Murilo César Ferreira <mucefe@yahoo.com.br>
2023-02-01 17:20:43 -0300

    Adicionei o Exercício 02

---
+++ Aulas Particulares/src/metodos/Exercicio02.java
@@ -0,0 +1,5 @@
+/*a) Método sem parâmetros e um método dentro do outro
+b) O método deve retornar um valor int. Faltou o "return"
+c) Variável 'a' declarada duas vezes. 
+d) Um método com retorno vazio (void) não pode retornar nada.*/
+
