8d0878bc1fb1ebf7574f7ad45bbbe2a0f6db9c6d
Murilo César Ferreira <mucefe@yahoo.com.br>
2023-02-03 17:12:34 -0300

    Alterações no Exercício 07

--- Aulas Particulares/src/metodos/Exercicio07.java
+++ Aulas Particulares/src/metodos/Exercicio07.java
@@ -3,12 +3,12 @@ um dos dois números. Escreva um método mdc que retorna o máximo divisor comum
 inteiros. [Dica: você poderia querer utilizar o algoritmo de Euclides] Incorpore o método a um
 aplicativo que lê dois valores do usuário e exibe o resultado.*/
 
-
 package metodos;
 
 import java.util.Scanner;
 
 public class Exercicio07 {
+	private static int[] primos = { 2, 3, 5, 7, 11, 13, 17, 19 };
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -18,27 +18,42 @@ public class Exercicio07 {
 		int num2 = sc.nextInt();
 		System.out.println("O Máximo Divisor Comum (MDC) de " + num1 + " e " + num2 + " é: " + mdc(num1, num2));
 		sc.close();
+
 	}
 
 	public static int mdc(int n1, int n2) {
-		int mdc = 0;
-		if (n1 > n2) {
-			for (int i = 1; i < n1; i++) {
-				if (i % n1 == 0 && i % n2 == 0 && i > mdc) {
-					mdc += i;
-
-				}
-			}
+		int mdc = 1;
+		if (n1<1||n2<1) {
+			return -1;
 		}
-		if (n2 > n1) {
-			for (int i = 1; i < n2; i++) {
-				if (i % n1 == 0 && i % n2 == 0 && i > mdc) {
-					mdc += i;
-
-				}
-			}
-
+		for (int i = 0; i < primos.length;) {
+			int contador=0;
+		if (n1%primos[i]==0) {
+			n1/=primos[i];
+			contador++;
 		}
+		if (n2%primos[i]==0) {
+			n2/=primos[i];
+			contador++;
+		}
+		if (contador==2) {
+			mdc*=primos[i];
+		}
+		if (contador==0) {
+			i++;
+			
+		}
+		if (n1==1 && n2==1) {
+			break;
+		}
+		}
+//		for (int i = 1; i < n1 || i < n2; i++) {
+//			if (n1 % i == 0 && n2 % i == 0) {
+//				mdc *= i;
+//
+//			}
+//		}
+
 		return mdc;
 	}
 }
