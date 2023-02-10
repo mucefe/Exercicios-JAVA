ba04d4ef348d65ffcb9c1f153ebbc46fe7991f12
Murilo César Ferreira <mucefe@yahoo.com.br>
2023-02-10 16:00:09 -0300

    Adicionei testes extras às funções do Exercicio05

--- Aulas Particulares/src/testes/TesteExercicio05.java
+++ Aulas Particulares/src/testes/TesteExercicio05.java
@@ -1,6 +1,6 @@
 package testes;
 
-import static org.junit.Assert.assertEquals; 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import metodos.Exercicio05;
@@ -8,12 +8,24 @@ import metodos.Exercicio05;
 public class TesteExercicio05 {
 
 	@Test
-	public void inverteString() {
-		assertEquals(4321, Exercicio05.inverteString(1234));
+	public void inverteTudo() {
+		assertEquals("4321", Exercicio05.inverteString(1234));
+		assertEquals("4321", Exercicio05.inverteNumeros(1234));
 	}
 
 	@Test
-	public void inverteNumeros() {
-		assertEquals(4321, Exercicio05.inverteNumeros(1234));
+	public void inverte1() {
+		assertEquals("1", Exercicio05.inverteNumeros(1));
+		assertEquals("1", Exercicio05.inverteString(1));
+	}
+	@Test
+	public void inverte100() {
+		assertEquals("001", Exercicio05.inverteNumeros(100));
+		assertEquals("001", Exercicio05.inverteString(100));
+	}
+	@Test
+	public void inverte120() {
+		assertEquals("021", Exercicio05.inverteNumeros(120));
+		assertEquals("021", Exercicio05.inverteString(120));
 	}
 }
