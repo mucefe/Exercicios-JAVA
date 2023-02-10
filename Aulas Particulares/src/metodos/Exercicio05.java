6f574545b9fec7b4d67dc394c1c569cdc3fb9d5a
Murilo César Ferreira <mucefe@yahoo.com.br>
2023-02-10 16:02:05 -0300

    Adicionei alterações ao exercício 05

--- Aulas Particulares/src/metodos/Exercicio05.java
+++ Aulas Particulares/src/metodos/Exercicio05.java
@@ -1,4 +1,4 @@
-/*Escreva um método que recebe um valor inteiro e retorna o número com seus dígitos invertidos. 
+/*Escreva um método que recebe um valor inteiro e retorna o número com seus dígitos invertidos.  
 Por exemplo, dado o número 7.631, o método deve retornar 1.367. Incorpore o método a um
 aplicativo que lê um valor a partir da entrada fornecida pelo usuário e exibe o resultado.*/
 
@@ -19,24 +19,28 @@ public class Exercicio05 {
 	entrada.close();
 	}
 	
-	public static void inverteString(int numero) {
+	public static String inverteString(int numero) {
 		char[] caracteres=Integer.toString(numero).toCharArray();
+		String numeroInvertido="";
 		for (int i=caracteres.length-1;i>=0;i--) {
-			System.out.print(caracteres[i]);
+			numeroInvertido+=caracteres[i];
 		}
+		return numeroInvertido;
 	}
 	
-	public static void inverteNumeros(int numero) {
+	public static String inverteNumeros(int numero) {
 
 		int i = 0;
+		int contador=0;
 
 		while (numero > 0) {
 			i *= 10; 
 			i += (numero % 10);
 			numero /= 10;
+			contador++;
 		}
 
-		System.out.printf("O número invertido é: %d.\n", i);
+		return String.format("%0"+contador+"d",i);
 
 	}
 
