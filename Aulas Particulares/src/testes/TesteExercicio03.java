5e8379425f9946dc5965e5b7bf337f29619f7e5a
Murilo César Ferreira <mucefe@yahoo.com.br>
2023-02-10 15:59:30 -0300

    Adicionei testes extras às funções do Exercicio03

--- Aulas Particulares/src/testes/TesteExercicio03.java
+++ Aulas Particulares/src/testes/TesteExercicio03.java
@@ -11,4 +11,17 @@ public class TesteExercicio03 {
 	public void testeDisplayDigits() {
 		assertEquals("7 8 2 4",Exercicio03.displayDigits(7824)); //Acho q ñ funciona pq é String, mas n sei como consertar
 	}
+	@Test
+	public void testeDisplayDigits2() {
+		assertEquals("O número deve estar entre 1 e 99999.",Exercicio03.displayDigits(0));
+	}
+	@Test
+	
+	public void testeDisplayDigits3() {
+		assertEquals("O número deve estar entre 1 e 99999.",Exercicio03.displayDigits(100000));
+	}
+	@Test
+	public void testeDisplayDigits4() {
+		assertEquals("1",Exercicio03.displayDigits(1));
+	}
 }
