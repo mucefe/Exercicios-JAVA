3ef0fd6e2e4907d98c3fd0e2b83e257323863116
Murilo César Ferreira <mucefe@yahoo.com.br>
2023-02-10 15:58:58 -0300

    Adicionei testes extras às funções do Exercicio01

--- Aulas Particulares/src/testes/TesteExercicio01.java
+++ Aulas Particulares/src/testes/TesteExercicio01.java
@@ -1,6 +1,6 @@
 package testes;
 
-import static org.junit.Assert.assertEquals; 
+import static org.junit.Assert.assertEquals;  
 import org.junit.Test;
 
 import metodos.Exercicio01;
@@ -9,11 +9,24 @@ import metodos.Exercicio01;
 public class TesteExercicio01 {
 
 	@Test
-	public void testeAnosParaDias(){
-		assertEquals(1095,Exercicio01.anosParaDias(3));
+	public void tudoParaDias(){
+		assertEquals(1170,Exercicio01.tudoParaDias(3,2,15));
 }
-	@Test 
-	public void testeMesesParaDias() {
-		assertEquals(360,Exercicio01.mesesParaDias(12)); //Não era pra ser 365?
+	@Test
+	public void tudoParaDias2(){
+		assertEquals(75,Exercicio01.tudoParaDias(0,2,15));
+}
+	@Test
+	public void tudoParaDias3(){
+		assertEquals(15,Exercicio01.tudoParaDias(0,0,15));
+}
+	@Test
+	public void tudoParaDias4(){
+		assertEquals(-1,Exercicio01.tudoParaDias(-1,0,0));
+}
+	@Test
+	public void tudoParaDias5() {
+		assertEquals(-1,Exercicio01.tudoParaDias(-1,-1,-1));
 	}
 }
+	
