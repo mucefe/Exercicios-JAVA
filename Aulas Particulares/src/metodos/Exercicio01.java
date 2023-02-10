c0766e4c64af1cf83ffc0f9c4cac1ceb5a7eeea9
Murilo César Ferreira <mucefe@yahoo.com.br>
2023-02-10 16:01:18 -0300

    Adicionei alterações ao exercício 01

--- Aulas Particulares/src/metodos/Exercicio01.java
+++ Aulas Particulares/src/metodos/Exercicio01.java
@@ -1,4 +1,4 @@
-/*Faça um método que receba a idade de uma pessoa expressa em anos, meses e dias e mostre-a
+/*Faça um método que receba a idade de uma pessoa expressa em anos, meses e dias e mostre-a 
 expressa em dias. Leve em consideraç˜ao o ano com 365 dias e o mês com 30. (Ex: 3 anos, 2
 meses e 15 dias = 1170 dias.)*/
 
@@ -16,21 +16,28 @@ public class Exercicio01 {
 		int meses = sc.nextInt();
 		System.out.print("Quantos dias? ");
 		int dias = sc.nextInt();
-		int diasTotais=anosParaDias(anos);
-		diasTotais+=mesesParaDias(meses);
-		diasTotais+=dias;
-		System.err.println("Idade total em dias: "+diasTotais);
+		
+		System.out.println("Idade total em dias: "+tudoParaDias(anos,meses,dias));
 		
 		sc.close();
 	}
+	public static int tudoParaDias(int anos,int meses,int dias) {
+		if (anos<0||meses<0||dias<0) {
+			return-1;
+		}
+		int diasTotais=anosParaDias(anos);
+		diasTotais+=mesesParaDias(meses);
+		diasTotais+=dias;
+		return diasTotais;
+	}
 
-	public static int anosParaDias(int anos) {
+	private static int anosParaDias(int anos) {
 		int dias=anos*365;
 		return dias;
 		
 	
 	}
-	public static int mesesParaDias(int meses) {
+	private static int mesesParaDias(int meses) {
 		int dias=meses*30;
 		return dias;
 	}
