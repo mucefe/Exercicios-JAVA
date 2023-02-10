486b162a0c59dadd5f32d9fade9fbbe2c25e2cf0
Murilo César Ferreira <mucefe@yahoo.com.br>
2023-02-10 15:59:55 -0300

    Adicionei testes extras às funções do Exercicio04

--- Aulas Particulares/src/testes/TesteExercicio04.java
+++ Aulas Particulares/src/testes/TesteExercicio04.java
@@ -10,6 +10,28 @@ public class TesteExercicio04 {
 	@Test
 	public void testeVerificaPrimo() {
 		assertEquals(true,Exercicio04.verificaPrimo(5));
+		assertEquals(true,Exercicio04.verificaPrimo(3));
+		assertEquals(true,Exercicio04.verificaPrimo(2));
+		assertEquals(true,Exercicio04.verificaPrimo(7));
+		assertEquals(true,Exercicio04.verificaPrimo(11));
+		assertEquals(true,Exercicio04.verificaPrimo(13));
 	}
+	@Test
+	public void testeVerificaNaoPrimo2() {
+		assertEquals(false,Exercicio04.verificaPrimo(4));
+		assertEquals(false,Exercicio04.verificaPrimo(1));
+		assertEquals(false,Exercicio04.verificaPrimo(6));
+		assertEquals(false,Exercicio04.verificaPrimo(8));
+		assertEquals(false,Exercicio04.verificaPrimo(9));
+	}
+	@Test
+	public void testeVerificaZero() {
+		assertEquals(false,Exercicio04.verificaPrimo(0));
+	}
+	@Test
+	public void testeNegativo() {
+		assertEquals(false,Exercicio04.verificaPrimo(-1));
+	}
+
 
 }
