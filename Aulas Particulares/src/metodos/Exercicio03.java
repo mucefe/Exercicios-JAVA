b9611c239c840cba5c588cb8557e87d7433735b7
Murilo César Ferreira <mucefe@yahoo.com.br>
2023-02-10 16:01:37 -0300

    Adicionei alterações ao exercício 03

--- Aulas Particulares/src/metodos/Exercicio03.java
+++ Aulas Particulares/src/metodos/Exercicio03.java
@@ -1,4 +1,4 @@
-/*Utilize métodos desenvolvidos nas partes (a) e (b) para escrever um método displayDigits que 
+/*Utilize métodos desenvolvidos nas partes (a) e (b) para escrever um método displayDigits que  
 recebe um inteiro entre 1 e 99999 e o exibe como uma seqüência de dígitos, separando cada par
 de dígitos por dois espaços. Por exemplo, o inteiro 4562 deve aparecer como ”4 5 6 2”.*/
 
@@ -12,20 +12,22 @@ public class Exercicio03 {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Insira um número entre 1 e 99999.");
 		int n = sc.nextInt();
-		displayDigits(n);
+		System.out.println(displayDigits(n));
 		sc.close();
 	}
 
-	public static void displayDigits(int num) {
+	public static String displayDigits(int num) {
 		if (num >= 1 && num <= 99999) {
 			String numeros = Integer.toString(num);
+			String numeroRetornavel="";
 
 			for (int i = 0; i < numeros.length(); i++) {
-				System.out.print(numeros.charAt(i) + " ");
+				numeroRetornavel+=numeros.charAt(i) + " ";
 			}
-
+			return numeroRetornavel.trim();
 		} else {
-			System.out.println("O número deve estar entre 1 e 99999.");
+			return "O número deve estar entre 1 e 99999.";
 		}
+
 	}
 }
